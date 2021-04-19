a6f00fc1987490e634f08e9a8f194b53413433b7
Otacilio Neto <otaciliomanoel.neto@hotmail.com>
2021-04-19 19:59:54 -0300

    add global style

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,9 +1,11 @@
+import { GlobalStyle } from '../../styles/global';
 import { Container } from './styles';
 
 export function App() {
   return (
     <Container>
       <h1>App</h1>
+      < GlobalStyle />
     </Container>
   );
 };

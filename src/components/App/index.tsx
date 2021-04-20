16742836c04881ad86981991788ee4e7ccbdc426
Otacilio Neto <otaciliomanoel.neto@hotmail.com>
2021-04-20 08:03:53 -0300

    add dashboard, transactionsTable, Summary

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,10 +1,12 @@
 import { GlobalStyle } from '../../styles/global';
 import { Header } from '../Header';
+import { Dashboard } from '../Dashboard'
 
 export function App() {
   return (
     <>
       <Header />
+      <Dashboard />
       <GlobalStyle />
     </>
   );
---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,25 @@
+import { Container } from './styles';
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Titulo</th>
+            <th>Titulo</th>
+            <th>Titulo</th>
+            <th>Titulo</th>
+          </tr>
+
+        </thead>
+        <tbody>
+          <td>Titulo</td>
+          <td>Titulo</td>
+          <td>Titulo</td>
+          <td>Titulo</td>
+        </tbody>
+      </table>
+    </Container>
+  );
+};
